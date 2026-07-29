c1a368380e6779ca7d9b389ad3d5d5afb5fce063
Viktor <gribanoviktor@gmail.com>
2026-07-30 01:29:00 +0300

    Fix: use PORT env var, disable debug

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+import os
 from flask import Flask, render_template, jsonify
-import random
 
 app = Flask(__name__)
 
@@ -225,4 +225,5 @@ def get_card(card_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
